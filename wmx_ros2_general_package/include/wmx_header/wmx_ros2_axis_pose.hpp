a470fab55d52dd901df4afa68358c43bdc8bc951
mfikih <mfikih@movensys.com>
2025-06-24 15:23:16 +0800

    add execute

--- wmx_ros2_general_package/include/wmx_header/wmx_ros2_axis_pose.hpp
+++ wmx_ros2_general_package/include/wmx_header/wmx_ros2_axis_pose.hpp
@@ -1,12 +1,19 @@
 #include <functional>
 #include <memory>
 #include <thread>
+#include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
 #include "wmx_ros2_message/action/axis_pose.hpp"
+
+#include "WMX3Api.h"
+#include "CoreMotionApi.h"
+
+using namespace wmx3Api;
+using namespace std;
 
 class WmxRos2AxisPoseServer : public rclcpp::Node
 {
@@ -28,6 +35,17 @@ class WmxRos2AxisPoseServer : public rclcpp::Node
     }
 
   private:
+    int err_;
+    char errString_[256];  
+    char buffer_[512];
+    int rate_ = 10;
+
+    WMX3Api wmx3Lib_;    
+    CoreMotion wmx3LibCm_;    
+    CoreMotionStatus cmStatus_;
+
+    wmx3Api::Position::PoseCommand position_ = wmx3Api::Position::PoseCommand();
+
     rclcpp_action::Server<AxisPose>::SharedPtr wmx_ros2_axis_pose_;
 
     rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const AxisPose::Goal> goal)
@@ -52,13 +70,60 @@ class WmxRos2AxisPoseServer : public rclcpp::Node
 
     void execute(const std::shared_ptr<GoalHandleAxisPose> goal_handle)
     {
-      RCLCPP_INFO(this->get_logger(), "Executing moving axis");
-      rclcpp::Rate loop_rate(10);
+      RCLCPP_INFO(this->get_logger(), "Executing moving axis pose");
+      rclcpp::Rate loop_rate(rate_);
 
       const auto goal = goal_handle->get_goal();
       auto feedback = std::make_shared<AxisPose::Feedback>();
       auto result = std::make_shared<AxisPose::Result>();
 
-      
+      wmx3LibCm_.GetStatus(&cmStatus_);
+      feedback->current = cmStatus_.axesStatus[goal->index].actualVelocity;
+
+      while(std::abs(goal->target - feedback->current) <= goal->threshold){
+        wmx3LibCm_.GetStatus(&cmStatus_);
+        feedback->current = cmStatus_.axesStatus[goal->index].actualVelocity;
+
+        if (goal_handle->is_canceling()) {
+          snprintf(buffer_, sizeof(buffer_), "Moving axis pose is canceled");
+          RCLCPP_INFO(this->get_logger(), "%s", buffer_);
+          result->succes = false;
+          result->final = feedback->current;
+          result->message = std::string(buffer_);
+          goal_handle->canceled(result);
+          return;
+        }
+
+        position_.axis = goal->index;
+        position_.profile.pose = goal->pose;
+        position_.profile.velocity = goal->velocity;
+        position_.profile.type = ProfileType::T::Trapezoidal; //goal->profile
+        position_.profile.acc = goal->acc;
+        position_.profile.dec = goal->dec;
+
+        err_ = wmx3LibCm_.position->move(&position_);
+
+        if (err_ != ErrorCode::None) {
+          snprintf(buffer_, sizeof(buffer_), "Failed to moving pose axis %d", goal->index);
+          RCLCPP_ERROR(this->get_logger(), "%s", buffer_);
+          response->success = false;
+          result->final = feedback->current;
+          response->message = std::string(buffer_);
+          goal_handle->abort(result)
+        }
+
+        
+        goal_handle->publish_feedback(feedback);
+        loop_rate.sleep();
+      }
+
+      if (rclcpp::ok()) {
+        snprintf(buffer_, sizeof(buffer_), "Moving axis pose is succesed");
+        RCLCPP_INFO(this->get_logger(), "%s", buffer_);
+        result->succes = true;
+        result->final = feedback->current;
+        result->message = std::string(buffer_);
+        goal_handle->succeed(result);
+      }
     }
 }; 
