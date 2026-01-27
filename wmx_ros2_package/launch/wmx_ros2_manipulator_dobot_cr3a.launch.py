ddf3df098020b3772d576dbf9e2e7ce59d469d0c
mfikih <mfikih@movensys.com>
2026-01-27 01:07:11 +0000

    add sim time

--- wmx_ros2_package/launch/wmx_ros2_manipulator_dobot_cr3a.launch.py
+++ wmx_ros2_package/launch/wmx_ros2_manipulator_dobot_cr3a.launch.py
@@ -1,9 +1,13 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
     config = os.path.join(
         get_package_share_directory('wmx_ros2_package'),
         'config',
@@ -11,12 +15,13 @@ def generate_launch_description():
     )
 
     start_manipulator_state = Node(package='wmx_ros2_package', executable='manipulator_state', name='manipulator_state',
-                                parameters=[config], output='screen')
-    
-    start_follow_joint_trajectory_server = Node(package='wmx_ros2_package', executable='follow_joint_trajectory_server', 
-                                name='follow_joint_trajectory_server', parameters=[config], output='screen')
+                                parameters=[config, {'use_sim_time': use_sim_time}], output='screen')
+
+    start_follow_joint_trajectory_server = Node(package='wmx_ros2_package', executable='follow_joint_trajectory_server',
+                                name='follow_joint_trajectory_server', parameters=[config, {'use_sim_time': use_sim_time}], output='screen')
 
     return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false', description='Use simulation clock if true'),
         start_manipulator_state,
         start_follow_joint_trajectory_server
     ])
