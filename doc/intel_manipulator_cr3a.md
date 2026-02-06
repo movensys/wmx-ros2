865ef2d782bd58f5378ce538ff68ead87e7bd903
mfikih <mfikih@movensys.com>
2026-02-06 10:30:08 +0900

    separate intel and jetson

---
+++ doc/intel_manipulator_cr3a.md
@@ -0,0 +1,16 @@
+## For trajectory control
+
+Manipulator Launch
+```
+sudo --preserve-env=PATH \
+     --preserve-env=AMENT_PREFIX_PATH \
+     --preserve-env=COLCON_PREFIX_PATH \
+     --preserve-env=PYTHONPATH \
+     --preserve-env=LD_LIBRARY_PATH \
+     --preserve-env=ROS_DISTRO \
+     --preserve-env=ROS_VERSION \
+     --preserve-env=ROS_PYTHON_VERSION \
+     --preserve-env=ROS_DOMAIN_ID \
+     --preserve-env=RMW_IMPLEMENTATION \
+     bash -c "source /opt/ros/jazzy/setup.bash && source /home/mvsk/wmx_ros2_ws/install/setup.bash && ros2 launch wmx_ros2_package wmx_ros2_manipulator_dobot_cr3a.launch.py use_sim_time:=false"
+```
---
+++ wmx_ros2_package/launch/wmx_ros2_jetson_manipulator_cr3a.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    config = os.path.join(
+        get_package_share_directory('wmx_ros2_package'),
+        'config',
+        'manipulator_config_dobot_cr3a.yaml'
+    )
+
+    start_manipulator_state = Node(package='wmx_ros2_package', executable='manipulator_state', name='manipulator_state',
+                                parameters=[config, {'use_sim_time': use_sim_time}], output='screen')
+
+    start_follow_joint_trajectory_server = Node(package='wmx_ros2_package', executable='follow_joint_trajectory_server',
+                                name='follow_joint_trajectory_server', parameters=[config, {'use_sim_time': use_sim_time}], output='screen')
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false', description='Use simulation clock if true'),
+        start_manipulator_state,
+        start_follow_joint_trajectory_server
+    ])
